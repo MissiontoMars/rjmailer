ce15cd481212a641f1ac1af3c9435a949b756de8
Noa Resare <noa@resare.com>
2009-04-06 19:55:12 +0200

    Added a reasonable logging setup

--- test/src/com/voxbiblia/rjmailer/TestBase.java
+++ test/src/com/voxbiblia/rjmailer/TestBase.java
@@ -2,9 +2,9 @@ package com.voxbiblia.rjmailer;
 
 import junit.framework.TestCase;
 
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
 
 /**
  * Common base class of all tests.
@@ -13,6 +13,26 @@ public abstract class TestBase
     extends TestCase
 {
     private static boolean loggingConfigured = false;
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
+    private static final String lineSep = System.getProperty("line.separator");
+
+    private static class LogFormatter extends Formatter
+    {
+        public String format(LogRecord logRecord)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(sdf.format(new Date()));
+            sb.append(" ");
+            String[] classParts = logRecord.getSourceClassName().split("\\.");
+            sb.append(classParts[classParts.length - 1]);
+            sb.append(" ");
+            sb.append(logRecord.getLevel());
+            sb.append(" ");
+            sb.append(logRecord.getMessage());
+            sb.append(lineSep);
+            return sb.toString();
+        }
+    }
 
     @Override
     protected void setUp() throws Exception
@@ -22,10 +42,13 @@ public abstract class TestBase
 
     private static void setupLogging()
     {
+
         if (!loggingConfigured) {
+            LogFormatter lf = new LogFormatter();
             Logger root = Logger.getLogger("");
             for (Handler h : root.getHandlers()) {
                 h.setLevel(Level.ALL);
+                h.setFormatter(lf);
             }
             root.setLevel(Level.FINEST);
             loggingConfigured = true;
